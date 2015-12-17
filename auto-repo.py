c918ceaf3748cf34e21b3cb47063fb89d945d999
daj0ker <vijaysgr8@gmail.com>
2015-12-17 07:12:20 +0000

    Initial Commit

---
+++ auto-repo.py
@@ -0,0 +1,73 @@
+#! /usr/bin/env python
+
+'''
+
+Usage
+
+   python auto-repo.py username repo
+
+'''
+
+import os
+
+import sys
+
+ 
+
+args = sys.argv
+
+ 
+
+if len(args) <= 2:
+
+    print "Usage: python auto-repo.py username repo"
+
+    exit()
+
+ 
+
+handle = args[1]
+
+repo = args[2]
+
+ 
+
+try:
+
+    os.chdir(repo)
+
+except:
+
+    print repo + " does not exist."
+
+    exit()
+
+ 
+
+url = "https://github.com/" + handle + "/"
+
+ 
+
+cmds = [
+
+    "git init",
+
+    "git add .",
+
+    "git commit -m \"Initial Commit\"",
+
+    "curl -u " + handle +  " https://api.github.com/user/repos -d " + "\'{\"name\":" + "\"" + repo + "\"}\'",
+
+    "git remote add origin git@github.com:" + handle + "/" + repo + ".git",
+
+    "git push origin master"
+
+    ]
+
+ 
+
+for cmd in cmds:
+
+    os.system(cmd)
+
+
